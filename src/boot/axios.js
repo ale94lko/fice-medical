eb748aeb6325544adf8cde3f6915a9e79f6b6caa
Alejandro Fernandez <47539231+ale94lko@users.noreply.github.com>
2025-08-06 09:45:16 -0400

    Fix axios configuration for making API requests

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,18 +1,14 @@
 import { defineBoot } from '#q-app/wrappers'
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'https://api.example.com' })
+const api = axios.create({ baseURL: 'https://randomuser.me/api/' })
 
 export default defineBoot(({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
-
   app.config.globalProperties.$axios = axios
-  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
-  //       so you won't necessarily have to import axios in each vue file
-
   app.config.globalProperties.$api = api
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
-  //       so you can easily perform requests against your app's API
+
+  // Para Composition API (provide/inject)
+  app.provide('api', api)
 })
 
-export { api }
+export const apiInstance = api
