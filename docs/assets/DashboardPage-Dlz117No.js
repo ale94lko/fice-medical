48a57f76662d231676ed4baad8bb91ec8ab4ecd4
ale94lko <ale94lko@users.noreply.github.com>
2026-05-23 16:37:26 +0000

    chore: update docs

---
+++ docs/assets/DashboardPage-Dlz117No.js
@@ -0,0 +1,1 @@
+import{It as e,J as t,nn as n}from"./index-CmJI5vJD.js";import{t as r}from"./QPage-DLu4Wiym.js";var i={};function a(t,i){return n(),e(r,{class:`flex flex-center`})}var o=t(i,[[`render`,a]]);export{o as default};
