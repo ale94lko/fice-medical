5c389a8d8fbb2c152ec1169fca3b8c950ee3876d
ale94lko <ale94lko@users.noreply.github.com>
2026-05-25 18:22:18 +0000

    chore: update docs

--- docs/assets/DashboardPage-B38dyDhs.js
+++
@@ -1,1 +0,0 @@
-import{Cn as e,Ft as t,Un as n}from"./index-fPEJjk_O.js";import{t as r}from"./QPage-BaB6372k.js";var i={};function a(t,i){return n(),e(r,{class:`flex flex-center`})}var o=t(i,[[`render`,a]]);export{o as default};
