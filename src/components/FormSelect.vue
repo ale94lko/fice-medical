2ca4c59e0dcbd8cd86d7323afae96d31a2f74461
Fidel Alejandro Fernandez Arias <47539231+ale94lko@users.noreply.github.com>
2026-05-31 16:58:05 +0400

    Fix

--- src/components/FormSelect.vue
+++ src/components/FormSelect.vue
@@ -3,7 +3,7 @@
     v-bind="forwardedAttrs"
     :model-value="modelValue"
     :clearable="showClearable"
-    @update:model-value="emit('update:modelValue', $event)"
+    @update:model-value="onUpdate"
   />
 </template>
 
@@ -19,22 +19,29 @@ const props = defineProps({
   modelValue: {
     default: null,
   },
+  /** When true, shows clear icon only while a value is selected. */
+  clearable: {
+    type: Boolean,
+    default: true,
+  },
 })
 
 const emit = defineEmits(['update:modelValue'])
 
 const attrs = useAttrs()
 
-const wantsClearable = computed(() => Boolean(attrs.clearable))
-
-const forwardedAttrs = computed(() => {
-  const rest = { ...attrs }
-  delete rest.clearable
-
-  return rest
-})
+const forwardedAttrs = computed(() => ({ ...attrs }))
 
 const showClearable = computed(
-  () => wantsClearable.value && hasSelectValue(props.modelValue),
+  () => props.clearable && hasSelectValue(props.modelValue),
 )
+
+function onUpdate(value) {
+  if (value === '' || value === undefined) {
+    emit('update:modelValue', null)
+
+    return
+  }
+  emit('update:modelValue', value)
+}
 </script>
